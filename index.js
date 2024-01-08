ddbe397662a5afd21ab2629815b031d520b0f819
Petermwansa <mwansapeter800@gmail.com>
2024-01-08 16:38:05 +0300

    Make the entered data to be stored in the local storage so that it can still be displayed when the browser is refreshed or closed and reopened

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const listContainer = document.getElementById("list__container")
 
 
 // when we click on the add button this code will be excecuted 
-function addTask () {
+addTask = () => {
 
     //here we are checking if the list is empty, it pops up an alert
     if (inputBox.value === '') {
@@ -19,12 +19,29 @@ function addTask () {
         li.appendChild(span)
     }
     inputBox.value = "";
+
+    // whenever we add any changes to our app, the function for saving them will be called 
+    saveData();
 }
 
 listContainer.addEventListener("click", (e) => {
     if (e.target.tagName === "LI") {
         e.target.classList.toggle("checked");
+        saveData();
     } else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove()
+        saveData();
     }
 }, false)
+
+//this function will save our data to the local storange 
+saveData = () => {
+    // it will save the innerHTML of the listContainer as data 
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+// this function is for displaying the saved data about whenever the browser is opened again 
+showTask = () => {
+    listContainer.innerHTML = localStorage.getItem("data")
+}
+showTask();
